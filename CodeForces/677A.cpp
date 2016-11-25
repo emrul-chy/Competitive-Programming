a3757165d88390bd68550a2cf418285e10a7f8be
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2016-11-25 15:55:57 +0600

    Create 677A.cpp

---
+++ CodeForces/677A.cpp
@@ -0,0 +1,18 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, c, h, t;
+    cin >> n >> h;
+    c = n;
+    while(n--)
+    {
+        cin >> t;
+        if( t > h) c++;
+    }
+    cout << c;
+    return 0;
+}
