ed7d2a6d71d7b375efd0683367286f32c42b2946
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2016-12-08 13:57:44 +0600

    Update 11946 - Code Number.cpp

--- UVa/11946 - Code Number.cpp
+++ UVa/11946 - Code Number.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 int main()
 {
-    int t, a[100006];
-    while(cin>>t)
+    char s[82], c[100];
+    c['0']='O', c['1']= 'I', c['2'] ='Z', c['3'] = 'E', c['4'] = 'A', c['5'] = 'S', c['6'] = 'G', c['7'] = 'T', c['8'] = 'B', c['9'] = 'P';
+    int t, i, j;
+    cin >> t;
+    getchar();
+    for(i=0; i<t; i++)
     {
-        if(t == 0)
+        while(gets(s))
         {
-            break;
-        }
-        int i, c =0;
-        for(i=1; i<=t; i++)
-        {
-            cin>>a[i];
-        }
-        a[0] = a[t];
-        a[t+1] = a[1];
-        for(i=1; i<t; i++)
-        {
-            if(a[i-1] > a[i] && a[i] < a[i+1] || a[i-1] < a[i] && a[i] > a[i+1])
+            if(strlen(s) == 0)
             {
-                c++;
+                break;
             }
+            for(j=0; s[j] != '\0'; j++)
+            {
+                if(s[j] >= '0' && s[j] <= '9')
+                {
+                    s[j] = c[s[j]];
+                }
+                printf("%c", s[j]);
+            }
+            printf("\n");
         }
-        cout<<c+1<<endl;
+        if(i != t- 1)
+        {
+            printf("\n");
+        }
     }
     return 0;
 }
-
