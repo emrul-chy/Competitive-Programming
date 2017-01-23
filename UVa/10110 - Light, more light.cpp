d9165e2159e23daceb2bc25002dac699ef3ceaf3
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-01-23 20:02:40 +0600

    Update 10110 - Light, more light.cpp

--- UVa/10110 - Light, more light.cpp
+++ UVa/10110 - Light, more light.cpp
@@ -2,30 +2,22 @@
 using namespace std;
 int main()
 {
-    long long int n;
-    cin >> n;
-    while(n > 5)
+    long long int i, t, n, sq;
+    while(cin>>n)
     {
-        n /= 2;
-        n -=2;
-    }
-    switch(n)
-    {
-    case 1:
-        cout << "Sheldon\n";
-        break;
-    case 2:
-        cout << "Leonard\n";
-        break;
-    case 3:
-        cout << "Penny\n";
-        break;
-    case 4:
-        cout << "Rajesh\n";
-        break;
-    case 5:
-        cout << "Howard\n";
-        break;
+        if(n == 0)
+        {
+            break;
+        }
+        sq = sqrt(n);
+        if(sq * sq == n)
+        {
+            printf("yes\n");
+        }
+        else
+        {
+            printf("no\n");
+        }
     }
     return 0;
 }
