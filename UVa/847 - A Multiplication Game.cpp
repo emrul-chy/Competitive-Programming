ce9197059f4457e0351bc924517b08815a098221
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-02-02 13:54:57 +0600

    Create 847 - A Multiplication Game.cpp

---
+++ UVa/847 - A Multiplication Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double n;
+    while(scanf("%lf", &n)==1)
+    {
+        while(n > 18)
+        {
+            n /= 18;
+        }
+        if(n <= 9)
+        {
+            printf("Stan wins.\n");
+        }
+        else
+        {
+            printf("Ollie wins.\n");
+        }
+    }
+    return 0;
+}
+
