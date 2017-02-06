2cf85bdbf05e15cc93df9dc4ac31bd1a2f6f00c3
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-02-07 00:32:29 +0600

    Create 13059 - Tennis Championship.cpp

---
+++ UVa/13059 - Tennis Championship.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int T;
+    
+    while(scanf("%lld", &T)==1)
+    {
+        T--;
+        printf("%lld\n");
+    }
+    
+    return 0;
+}
