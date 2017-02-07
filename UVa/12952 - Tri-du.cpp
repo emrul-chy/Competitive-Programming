40428f4ddb3d6dd4860fddae648066868a868973
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-02-07 12:28:15 +0600

    Create 12952 - Tri-du.cpp

---
+++ UVa/12952 - Tri-du.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while(scanf("%d %d", &a, &b) == 2)
+    {
+        printf("%d\n", (a <= b) ? b : a);
+    }
+    return 0;
+}
