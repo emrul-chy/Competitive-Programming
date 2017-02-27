225526a4c822c644c4ab89224c8366ec935f1030
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-02-27 19:08:08 +0600

    Update Number Sorting upto 10^6 digits.cpp

--- Number Sorting upto 10^6 digits.cpp
+++ Number Sorting upto 10^6 digits.cpp
@@ -15,25 +15,25 @@ int main()
     
     cin >> n;
     
-    for(i=0; i<n; i++)
+    for( i=0; i<n; i++ )
     {
         cin >> s;
-        if(mp[s.size()]==0)
+        if( mp[s.size()] == 0 )
         {
             v1.push_back(s.size());
         }
-        mp[s.size()]++;
-        v[s.size()].push_back(s);
+        mp[ s.size() ]++;
+        v[ s.size() ].push_back(s);
     }
     
-    sort(v1.begin(), v1.end());
+    sort( v1.begin(), v1.end());
     
-    for(i=0; i<v1.size(); i++)
+    for( i=0; i<v1.size(); i++)
     {
-        sort(v[v1[i]].begin(), v[v1[i]].end());
-        for(LL j=0; j<v[v1[i]].size(); j++)
+        sort( v[v1[i]].begin(), v[v1[i]].end() );
+        for( LL j=0; j<v[v1[i]].size(); j++ )
         {
-            cout<<v[v1[i]][j]<<endl;
+            cout << v[v1[i]][j] << endl;
         }
     }
     return 0;
