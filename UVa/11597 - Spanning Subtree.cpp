d914bd7782430adb138483c8eff94fe8f1cf2630
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-03-03 20:49:40 +0600

    Create 11597 - Spanning Subtree.cpp

---
+++ UVa/11597 - Spanning Subtree.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc = 1, t;
+    while(cin >> t, t)
+    {
+        printf("Case %d: %d\n", tc++, t/2);
+    }
+    return 0;
+}
