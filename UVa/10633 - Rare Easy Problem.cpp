39b9734d412b06070785a98fe70b67d50da6ecb4
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-03-08 00:20:02 +0600

    Update 10633 - Rare Easy Problem.cpp

--- UVa/10633 - Rare Easy Problem.cpp
+++ UVa/10633 - Rare Easy Problem.cpp
@@ -7,8 +7,10 @@ int main()
     LL n, x;
     while(scanf("%llu", &n) == 1)
     {
+        if(!n)
+            break;
         x = (n * 10) / 9;
-        if(n % 9 == 0)
+        if(!(n % 9))
             printf("%llu %llu\n", x-1, x);
         else
             printf("%llu\n", x);
